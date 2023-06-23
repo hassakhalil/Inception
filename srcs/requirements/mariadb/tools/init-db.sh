d01d7a7a534ec53ad79e212a90dc043544a64a80
hassan khalil <hassan@hassans-MacBook-Pro-2.local>
2023-06-23 19:57:26 +0100

    added root and other users password setup in the mariadb bash script

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -7,10 +7,23 @@ until mysqladmin ping -h db -u root -pexample >/dev/null 2>&1; do
     sleep 1
 done
 
+# Set root password
+mysql -h db -u root -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+# Set mysql user password
+mysql -h db -u root -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
 # Create the database
-mysql -h db -u root -pexample -e "CREATE DATABASE wordpress;"
+mysql -h db -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE wordpress;"
 
 # Create the user and grant privileges
-mysql -h db -u root -pexample -e "CREATE USER 'wordpress'@'%' IDENTIFIED BY 'password';"
-mysql -h db -u root -pexample -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';"
-mysql -h db -u root -pexample -e "FLUSH PRIVILEGES;"
+mysql -h db -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER 'wordpress'@'%' IDENTIFIED BY 'password';"
+mysql -h db -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';"
+mysql -h db -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+
+# Stop the MariaDB service
+mysqladmin shutdown
+
+# # Start MariaDB in the foreground
+# exec /usr/bin/mysqld_safe --bind-address=0.0.0.0
+echo "Database initialization complete!"
